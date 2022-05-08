371990385366879de6ed07919258973e3ad6b0d4
Alfredo Silva <ing.alfredo_silva@hotmail.com>
2022-05-08 11:19:40 -0500

    first commit

---
+++ assets/js/variables_y_propiedades.js
@@ -0,0 +1,27 @@
+/*=============================================
+VARIABLES DEL CANVAS
+=============================================*/
+var canvas;
+var ctx;
+
+/*=============================================
+PROPIEDADES DEL OBJETO DATOS
+=============================================*/
+
+var datos = {
+	nivel: null,
+	plano3: null,
+	plano2: null,
+	plano1: null,
+	plano0: null,
+	texturaPlataforma: null,
+	bloques:[],
+	imgJugador:null,
+	jugador_x:70,
+	jugador_y:200,
+	jugador_ancho:40,
+	jugador_alto:40,
+	detalles: null,
+	bloquesDetalles:[]
+
+}
